3bbf3c820eed0d71a0077fb093737425f76200f9
Yousef <usefz89@gmail.com>
2023-08-28 11:32:22 +0300

    Initial Commit

---
+++ ContactsBackUP/ContactsBackUPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsBackUPApp.swift
+//  ContactsBackUP
+//
+//  Created by Yousef Zuriqi on 28/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ContactsBackUPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
